4c5f03e088813d4ddb44d27af32f0710c4cb629b
Dolores Russo <drusso@alumnos.exa.unicen.edu.ar>
2025-03-16 00:28:36 -0300

    archivos de config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ["estaticos-cdn.prensaiberica.es", "ethic.es"], 
+    },
+  };
+  
+  export default nextConfig;

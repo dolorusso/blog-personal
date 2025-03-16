aaee6ed0758a7328321554663ef822855cddcef1
Dolores Russo <drusso@alumnos.exa.unicen.edu.ar>
2025-03-16 00:29:15 -0300

    archivos grales de la app

--- app/page.js
+++ app/page.js
@@ -1,101 +1,54 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="relative min-h-screen flex flex-col items-center justify-center">
+      <section className="flex items-center w-full max-w-6xl px-4 md:px-8 gap-0">
+        
+        <div className="flex flex-col items-center text-center gap-3 w-full">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold tracking-tighter bg-gradient-to-b from-[#305555] to-[#36e1f8] text-transparent bg-clip-text">
+            Dolores M. Russo
+          </h1>
+
+          <div className="flex justify-center gap-4 mt-2">
+            <a href="https://github.com/dolorusso" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} className="w-4 h-4 md:w-6 md:h-6 text-white hover:scale-110 transition-transform" />
+            </a>
+            <a href="https://x.com/dolorusso1" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} className="w-4 h-4 md:w-6 md:h-6 text-white hover:scale-110 transition-transform" />
+            </a>
+            <a href="https://instagram.com/dolo_russo" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} className="w-4 h-4 md:w-6 md:h-6 text-white hover:scale-110 transition-transform" />
+            </a>
+            <a href="mailto:dolo.mrusso@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} className="w-4 h-4 md:w-6 md:h-6 text-white hover:scale-110 transition-transform" />
+            </a>
+          </div>
+
+          <p className="text-base md:text-lg lg:text-xl text-[#4e8994] tracking-tight mt-4 max-w-[90%] md:max-w-[80%] lg:max-w-[70%] mx-auto">
+            No solo baso mi personalidad en estudiar Ing. de Sistemas, también me gusta escribir y por eso creé este blog.
+          </p>
+
+          <section className="flex justify-center mt-4">
+            <a
+              href="/articles"
+              className="px-3 py-2 md:px-4 md:py-3 text-sm md:text-base lg:text-base bg-gradient-to-b from-[#3a6868] to-[#36e1f8] text-[#121212] font-medium rounded-full transition-all duration-300 hover:scale-105 hover:brightness-110"
+            >
+              Ver Publicaciones
+            </a>
+          </section>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      <section id="articles" className="mt-16 w-full">
+        {/* carga de articulos */}
+      </section>
     </div>
   );
 }
+
